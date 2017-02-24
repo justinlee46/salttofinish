c831f86219e173045466b34ee842ce6cf55a12d1
Justin Lee <jlee4689@gmail.com>
2017-02-24 16:45:10 -0600

    salt scripts

---
+++ salt/scripts/docker_salt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker stop salty || echo 'container already stopped'
+docker rm salty || echo 'container already removed'
+docker build -t my_local_salt . --no-cache || exit 1
+# Run as privileged to enable docker within docker
+docker run --name salty -v "$PWD:/srv/salt" --privileged=true -d my_local_salt || exit 1
