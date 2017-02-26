06e8eb6d7eaf270615c5a8b14b73a025ee9ef1c2
Justin Lee <jlee4689@gmail.com>
2017-02-26 11:44:03 -0600

    images directory

--- salt/scripts/docker_salt.sh
+++ salt/scripts/docker_salt.sh
@@ -2,5 +2,4 @@
 docker stop salty || echo 'container already stopped'
 docker rm salty || echo 'container already removed'
 docker build -t my_local_salt . || exit 1
-# Run as privileged to enable docker within docker
-docker run --name salty -v "$PWD:/srv/salt" --privileged=true -d my_local_salt || exit 1
+docker run --name salty -v "$PWD:/srv/salt" -d my_local_salt || exit 1
